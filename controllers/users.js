b8f3c6b96d7be74e4959f2c5310a3887b14389ae
HanmaDaiki <daikihanma@outlook.com>
2022-09-10 14:35:39 +0300

    Обработчки ошибок для Пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -3,15 +3,20 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .catch(() => res.status(500).send({ message: 'ERROR :: Упс, у нас тут непредвиденная ошибка! Status(500)' }));
 };
 
 module.exports.getUserById = (req, res) => {
   const id = req.params.userId;
 
   User.find({ _id: id })
-    .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .then((user) => res.send({ data: { user } }))
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        return res.status(400).send({ message: 'ERROR :: Пользователь не найден! Status(400)' });
+      }
+      return res.status(500).send({ message: 'ERROR :: Упс, у нас тут непредвиденная ошибка! Status(500)' });
+    });
 };
 
 module.exports.createUser = (req, res) => {
@@ -19,23 +24,44 @@ module.exports.createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        return res.status(400).send({ message: 'ERROR :: Введены некорректные данные для создания пользователя! Status(400)' });
+      }
+      return res.status(500).send({ message: 'ERROR :: Упс, у нас тут непредвиденная ошибка! Status(500)' });
+    });
 };
 
 module.exports.updateUserInfo = (req, res) => {
-  const { name, about } = req.body;
   const userId = req.user._id;
+  const { name, about } = req.body;
 
-  User.updateOne({ _id: userId }, { name, about })
+  User.findByIdAndUpdate({ _id: userId }, { name, about }, { returnDocument: 'after', new: true })
     .then((user) => res.status(200).send({ data: user }))
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        return res.status(400).send({ message: 'ERROR :: Введены некорректные данные для обновления данных пользователя! Status(400)' });
+      }
+      if (error.name === 'CastError') {
+        return res.status(404).send({ message: 'ERROR :: Пользователь не найден! Status(404)' });
+      }
+      return res.status(500).send({ message: 'ERROR :: Упс, у нас тут непредвиденная ошибка! Status(500)' });
+    });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
 
-  User.updateOne({ _id: userId }, { avatar })
+  User.findByIdAndUpdate({ _id: userId }, { avatar }, { returnDocument: 'after', new: true })
     .then((user) => res.status(200).send({ data: user }))
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        return res.status(400).send({ message: 'ERROR :: Введены некорректные данные для обновления данных пользователя! Status(400)' });
+      }
+      if (error.name === 'CastError') {
+        return res.status(404).send({ message: 'ERROR :: Пользователь не найден! Status(404)' });
+      }
+      return res.status(500).send({ message: 'ERROR :: Упс, у нас тут непредвиденная ошибка! Status(500)' });
+    });
 };
